7afccfd267362bd7890bcddd0aefc60d28733953
Stupid <22520563+NewJerseyStyle@users.noreply.github.com>
2022-08-16 16:44:56 +0800

    beautified code & comments

--- MineShaft/cv_matching.py
+++ MineShaft/cv_matching.py
@@ -4,18 +4,21 @@ import numpy as np
 
 
 class cv_matching:
-    def __init__(self):
-        pass
-
     def preload_templates(self):
+        """Preload all images on start to optimize runtime
+
+        Images loaded:
+        - "../SourcePictures/findmatch2.png"
+        - "../SourcePictures/deathmatch2.png"
+        - "../SourcePictures/tutor.png"
+        - "../SourcePictures/entertutor2.png"
+        - "../SourcePictures/finishtutor.png"
         """
-        change the current directory to the script folder so that relative
-        path can be used
-        """
+        # ensure relative path
         folder_path = os.path.dirname(os.path.abspath(__file__))
         os.chdir(folder_path)
 
-        # opencv read the find match image by file path
+        # opencv read images for template matching
         self.findmatch2_png = cv2.imread(
             "../SourcePictures/findmatch2.png",
             cv2.IMREAD_UNCHANGED)
@@ -33,33 +36,23 @@ class cv_matching:
             cv2.IMREAD_UNCHANGED)
 
     def find_location(self, tofind):
-        """
-        the method requires an image parameter which is in numpy.ndarray format
-        then it compares the image with screen capture
-        """
+        """Fine the location of image on screen
 
- 
-        '''
-        screen capture using pyautogui 
-        '''
+        Instead of using `pyautogui.locateOnScreen()` OpenCV template matching
+        is used after `pyautogui.screenshot()`
+        """
+        # screen capture using pyautogui 
         screen = pyautogui.screenshot()
 
-        '''
-        change the capture to numpy array
-        '''
+        # change the capture to numpy array
         img = np.array(screen)
 
         '''
         since the matchtemplate() function requires 2 images with same format
         so the below code is to convert the images to RBG format
         '''
-
         tofind = cv.cvtColor(tofind, cv.COLOR_BGR2RGB)
-
-
-
         background = cv.cvtColor(img, cv.COLOR_BGR2RGB)
-
 
         '''
         the below opencv function matchTemplate() receive 3 parameters, first 2 are images in 
